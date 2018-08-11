42a0a14e5d6435a349c05856d06cdb31993bce54
e56 <edai28@gmail.com>
2018-08-11 14:48:30 -0400

    pull new eslint setup

--- client/src/app/app.jsx
+++ client/src/app/app.jsx
@@ -1,14 +1,36 @@
 <<<<<<< HEAD
 =======
 import React, { Fragment, Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      profileQueue: [
+        {
+          name: 'peter',
+          breed: 'golden corgie',
+          description: 'Lets skip the small talk and go for a walk',
+          picture: [],
+          location: '20057',
+        },
+      ],
+      date: new Date(),
+    };
   }
 
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <div>Hello World</div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <Button variant="contained" color="primary">
+          Like</Button>
+        <Button variant="contained" color="primary">
+          Don't Like</Button>
+      </div>
+    );
   }
 }
 
