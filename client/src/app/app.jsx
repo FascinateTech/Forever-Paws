c4189b654050de72dfc5464e3907448ed4f31454
e56 <edai28@gmail.com>
2018-08-13 14:17:52 -0400

    front end strawman edits

--- client/src/app/app.jsx
+++ client/src/app/app.jsx
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
-=======
-import React, { Fragment, Component } from 'react';
-import Button from '@material-ui/core/Button';
+import React, {Component} from 'react';
+// import Button from '@material-ui/core/Button';
+
+import PetProfile from './components/petProfile.jsx';
+import UserProfile from './components/userProfile.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -9,30 +10,80 @@ class App extends Component {
     this.state = {
       profileQueue: [
         {
-          name: 'peter',
+          name: 'Peter',
           breed: 'golden corgie',
+          age: '3 years old',
           description: 'Lets skip the small talk and go for a walk',
-          picture: [],
+          picture: ['https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg'],
           location: '20057',
+        }, 
+        {
+          name: 'Bones',
+          breed: 'bulldog',
+          age: '2 years old',
+          description: 'Lets play fetch',
+          picture: ['https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg'],
+          location: '45693',
+        }, 
+        {
+          name: 'Rover',
+          breed: 'golden corgie',
+          age: '1 years old',
+          description: 'Cute and fluffy',
+          picture: ['https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg'],
+          location: '82179',
+        }, 
+        {
+          name: 'Spot',
+          breed: 'pug',
+          age: '5 years old',
+          description: 'Friendly and playful',
+          picture: ['https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg'],
+          location: '90210',
         },
+        {
+          name: 'Last Dog',
+          breed: 'Last Dog',
+          age: 'Last Dog',
+          description: 'Last Dog',
+          picture: ['https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg'],
+          location: 'Last Dog',
+        }
       ],
       date: new Date(),
+      currentProfileIndex: 0,
+      currentProfileView: {}
     };
   }
+
+  componentDidMount() {
+    this.nextPet();
+  }
+
+  nextPet = () => { 
+    this.setState({
+      currentProfileView: this.state.profileQueue[this.state.currentProfileIndex],
+      currentProfileIndex: this.state.currentProfileIndex+1
+    })
+  }
+
+  tempStyleDELETE = {
+    margin: '5px',
+    border: '1px solid red'
+  };
 
   render() {
     return (
       <div>
-        <div>Hello World</div>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <Button variant="contained" color="primary">
-          Like</Button>
-        <Button variant="contained" color="primary">
-          Don't Like</Button>
+        <UserProfile />
+        <h2>Doggie Swipe {this.state.date.toLocaleTimeString()}.</h2>
+        <PetProfile profileQueue={this.state.currentProfileView}/>
+        <button type='submit' style={this.tempStyleDELETE} onClick={this.nextPet}>Like</button>
+        <button type='submit' style={this.tempStyleDELETE} onClick={this.nextPet}>Don't Like</button>
       </div>
     );
   }
 }
 
 export default App;
->>>>>>> edits
+
