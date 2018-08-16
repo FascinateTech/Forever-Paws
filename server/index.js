f1a2ede51e0d6980cac1e85554500bf999bccf00
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-08-16 14:48:33 -0700

    test

--- server/index.js
+++ server/index.js
@@ -3,4 +3,5 @@ import app from './app/app';
 const Console = console;
 
 const port = process.env.PORT || 3000;
+Console.log(process.env);
 app.listen(port, () => Console.log('listening on port: ', port));
