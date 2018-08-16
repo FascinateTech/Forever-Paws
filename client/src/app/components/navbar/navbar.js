a559cdb5082a20f07e92e0422e41d7f169b91532
Derrek Tran <blubby94@gmail.com>
2018-08-15 17:23:10 -0700

    added landing page and nav bar

---
+++ client/src/app/components/navbar/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import styled from 'styled-components';
+import { Container } from 'styled-container-component';
+import { Navbar, NavbarLink } from 'styled-navbar-component';
+import { Nav } from 'styled-nav-component';
+
+// const Nav = styled.a`
+//   position: fixed;
+//   width: 100%;
+//   backround: black;
+//   height: 56px;
+// `;
+const NavComponent = () => (
+  <Container fluid>
+    <Navbar expandSm light>
+      <Nav justified>
+        <NavbarLink>
+          <span>&#9776;</span>
+        </NavbarLink>
+        <NavbarLink light href="/">
+          <img src="./pawLogo.svg" alt="paw" />
+        </NavbarLink>
+        <NavbarLink light href="#">
+          Link
+        </NavbarLink>
+      </Nav>
+    </Navbar>
+  </Container>
+);
+
+export default NavComponent;
