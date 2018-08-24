a0951bd5b4c0c9afcb48a87625aca191cca8b01b
Derrek Tran <blubby94@gmail.com>
2018-08-24 01:55:41 -0700

    lint fix again

--- server/app/registerServiceWorker.js
+++ server/app/registerServiceWorker.js
@@ -3,7 +3,7 @@ const isLocalhost = Boolean(
     window.location.hostname === '[::1]' ||
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
-/*eslint no-param-reassign: "error" */
+/* eslint no-param-reassign: "error" */
 function registerValidSW(swUrl) {
   navigator.serviceWorker
     .register(swUrl)
