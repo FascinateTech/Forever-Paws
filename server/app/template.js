02ffdaf901affcb735a7b7558cdcde3f08e29e8f
Derrek Tran <blubby94@gmail.com>
2018-08-29 14:54:12 -0700

    finsihed camera upload feature

--- server/app/template.js
+++ server/app/template.js
@@ -18,7 +18,7 @@ export default html =>
         <script>
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function () {
-        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
           console.log('ServiceWorker registration failed: ', err);
