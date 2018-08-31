839ce751a2562e34c001185e84cf05ee85dd6050
Derrek Tran <blubby94@gmail.com>
2018-08-31 04:42:46 -0700

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'client/src'),
@@ -22,4 +24,26 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+    }),
+
+    new SWPrecacheWebpackPlugin({
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      logger(message) {
+        if (message.indexOf('Total precache size is') === 0) {
+          return;
+        }
+        if (message.indexOf('Skipping static resource') === 0) {
+          return;
+        }
+        console.log(message);
+      },
+      minify: true,
+      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    }),
+  ],
 };
