604359a05aebfd7f9016de0cc82d128c990ad867
derrek_tran <34522436+DerrekTran@users.noreply.github.com>
2018-08-31 05:32:25 -0700

    Update registerServiceWorker.js

--- client/src/app/registerServiceWorker.js
+++ client/src/app/registerServiceWorker.js
@@ -61,7 +61,7 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (!isLocalhost) {
         registerValidSW(swUrl);
